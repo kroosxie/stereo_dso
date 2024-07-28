1b0ba3bc0595d7e03942f1e635b2c28a616e52d4
Tse_Ubuntu <kroos_xie@126.com>
2024-07-28 06:59:11 -0700

    origin_code

---
+++ build/CMakeFiles/dso.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kroos/stereo_dso/src/FullSystem/CoarseInitializer.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/FullSystem/CoarseInitializer.cpp.o"
+  "/home/kroos/stereo_dso/src/FullSystem/CoarseTracker.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/FullSystem/CoarseTracker.cpp.o"
+  "/home/kroos/stereo_dso/src/FullSystem/FullSystem.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/FullSystem/FullSystem.cpp.o"
+  "/home/kroos/stereo_dso/src/FullSystem/FullSystemDebugStuff.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/FullSystem/FullSystemDebugStuff.cpp.o"
+  "/home/kroos/stereo_dso/src/FullSystem/FullSystemMarginalize.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/FullSystem/FullSystemMarginalize.cpp.o"
+  "/home/kroos/stereo_dso/src/FullSystem/FullSystemOptPoint.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/FullSystem/FullSystemOptPoint.cpp.o"
+  "/home/kroos/stereo_dso/src/FullSystem/FullSystemOptimize.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/FullSystem/FullSystemOptimize.cpp.o"
+  "/home/kroos/stereo_dso/src/FullSystem/HessianBlocks.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/FullSystem/HessianBlocks.cpp.o"
+  "/home/kroos/stereo_dso/src/FullSystem/ImmaturePoint.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/FullSystem/ImmaturePoint.cpp.o"
+  "/home/kroos/stereo_dso/src/FullSystem/PixelSelector2.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/FullSystem/PixelSelector2.cpp.o"
+  "/home/kroos/stereo_dso/src/FullSystem/Residuals.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/FullSystem/Residuals.cpp.o"
+  "/home/kroos/stereo_dso/src/IOWrapper/OpenCV/ImageDisplay_OpenCV.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/IOWrapper/OpenCV/ImageDisplay_OpenCV.cpp.o"
+  "/home/kroos/stereo_dso/src/IOWrapper/OpenCV/ImageRW_OpenCV.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/IOWrapper/OpenCV/ImageRW_OpenCV.cpp.o"
+  "/home/kroos/stereo_dso/src/IOWrapper/Pangolin/KeyFrameDisplay.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/IOWrapper/Pangolin/KeyFrameDisplay.cpp.o"
+  "/home/kroos/stereo_dso/src/IOWrapper/Pangolin/PangolinDSOViewer.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/IOWrapper/Pangolin/PangolinDSOViewer.cpp.o"
+  "/home/kroos/stereo_dso/src/OptimizationBackend/AccumulatedSCHessian.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/OptimizationBackend/AccumulatedSCHessian.cpp.o"
+  "/home/kroos/stereo_dso/src/OptimizationBackend/AccumulatedTopHessian.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/OptimizationBackend/AccumulatedTopHessian.cpp.o"
+  "/home/kroos/stereo_dso/src/OptimizationBackend/EnergyFunctional.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/OptimizationBackend/EnergyFunctional.cpp.o"
+  "/home/kroos/stereo_dso/src/OptimizationBackend/EnergyFunctionalStructs.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/OptimizationBackend/EnergyFunctionalStructs.cpp.o"
+  "/home/kroos/stereo_dso/src/util/Undistort.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/util/Undistort.cpp.o"
+  "/home/kroos/stereo_dso/src/util/globalCalib.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/util/globalCalib.cpp.o"
+  "/home/kroos/stereo_dso/src/util/settings.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/src/util/settings.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ENABLE_SSE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../thirdparty/Sophus"
+  "/usr/include/eigen3"
+  "/usr/local/lib/cmake/Pangolin/../../../include"
+  "/usr/include/opencv"
+  "/usr/include/suitesparse"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/dso_dataset.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kroos/stereo_dso/src/main_dso_pangolin.cpp" "/home/kroos/stereo_dso/build/CMakeFiles/dso_dataset.dir/src/main_dso_pangolin.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ENABLE_SSE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../thirdparty/Sophus"
+  "/usr/include/eigen3"
+  "/usr/local/lib/cmake/Pangolin/../../../include"
+  "/usr/include/opencv"
+  "/usr/include/suitesparse"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/kroos/stereo_dso/build/CMakeFiles/dso.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
