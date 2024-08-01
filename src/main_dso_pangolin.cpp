ea72987802aa229776272239e71f8861aefd2abd
Tse_Ubuntu <kroos_xie@126.com>
2024-08-01 01:55:31 -0700

    change the outputPath o result.txt

--- src/main_dso_pangolin.cpp
+++ src/main_dso_pangolin.cpp
@@ -556,7 +556,7 @@ int main( int argc, char** argv )
         gettimeofday(&tv_end, NULL);
 
 
-        fullSystem->printResult("/home/jiatianwu/project/sdso/result.txt");
+        fullSystem->printResult("/mnt/hgfs/ShareFolder/Beidou_farm/result.txt");
 
 
         int numFramesProcessed = abs(idsToPlay[0]-idsToPlay.back());
